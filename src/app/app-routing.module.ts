fe7f73b0bc2490292e8e77da6e3cf353e4949a88
KIRTISHD <aniket.dhande@gmail.com>
2020-07-18 22:48:31 +0530

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { GameComponent } from './game/game.component';
+import { FinishComponent } from './finish/finish.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'game', component: GameComponent, canActivate: [AuthGuard]},
+  { path: 'finish', component: FinishComponent, canActivate: [AuthGuard]},
+  { path: '', component: StartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { GameComponent } from './game/game.component';
+import { FinishComponent } from './finish/finish.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
+
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    GameComponent,
+    FinishComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
